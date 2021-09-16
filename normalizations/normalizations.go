78a8722ba7541deb8fc91c9238b36d1875c053a4
brookesargent <brookesargent01@gmail.com>
2021-09-16 16:44:25 -0400

    add a few normalizations and tests

--- normalizations/normalizations.go
+++ normalizations/normalizations.go
@@ -1,8 +1,37 @@
 package normalizations
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+	"time"
+)
 
 func FormatZipCode(zip string) string {
-	fmt.Println(zip)
-	return "0"
+	for len(zip) < 5 {
+		zip = fmt.Sprintf("%s%s", "0", zip)
+	}
+	return zip
+}
+
+func FormatFullName(name string) string {
+	return strings.ToUpper(name)
+}
+
+func DurationToSeconds(duration string) float64 {
+	splitDuration := strings.Split(duration, ":")
+	for i, v := range splitDuration {
+		switch i {
+		case 0:
+			splitDuration[i] = fmt.Sprintf("%sh", v)
+		case 1:
+			splitDuration[i] = fmt.Sprintf("%sm", v)
+		case 2:
+			splitDuration[i] = fmt.Sprintf("%sms", strings.Replace(v, ".", "s", 1))
+		}
+	}
+
+	duration = strings.Join(splitDuration, "")
+	parsedDuration, _ := time.ParseDuration(duration)
+
+	return parsedDuration.Seconds()
 }
--- normalizations/normalizations_test.go
+++ normalizations/normalizations_test.go
@@ -10,4 +10,24 @@ func TestFormatZipCode(t *testing.T) {
 	expected := "01125"
 	actual := FormatZipCode("1125")
 	assert.Equal(t, expected, actual)
+
+	expected = "00125"
+	actual = FormatZipCode("125")
+	assert.Equal(t, expected, actual)
+
+	expected = "11125"
+	actual = FormatZipCode("11125")
+	assert.Equal(t, expected, actual)
+}
+
+func TestFormatFullName(t *testing.T) {
+	expected := "BROOKE SARGENT"
+	actual := FormatFullName("brooke sargent")
+	assert.Equal(t, expected, actual)
+}
+
+func TestDurationToSeconds(t *testing.T) {
+	expected := 5012.123
+	actual := DurationToSeconds("1:23:32.123")
+	assert.Equal(t, expected, actual)
 }
